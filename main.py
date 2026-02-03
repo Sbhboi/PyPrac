d054b7e617eb71de8c6ca78972a709e2fc1feb53
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2026-02-04 00:06:26 +0800

    ....still debugging with TypeError at this point

--- main.py
+++ main.py
@@ -2,9 +2,31 @@ from ursina import *
 
 app = Ursina()
 
-player = Entity(model='cube',color=color.azure, scale=(1,2,1), collider='box', y=1)
+player = Entity(
+    model='cube',
+    color=color.azure, 
+    scale=(1,1,1), 
+    y=0,
+    z=0
+)
 
-ground = Entity(model='plane', scale=32, texture='white_cube', texture_scale=(32,32), color=color.lime)
+ground = Entity(
+    model='plane', 
+    scale=32, 
+    texture='white_cube', 
+    texture_scale=(32,32), 
+    color=color.lime,
+    rotation_x=90,
+    y=-0.5,
+    z=1
+)
+
+food = Entity(
+    model='sphere',
+    color=color.red,
+    scale=0.5,
+    position=(3,3,0)
+)
 
 def update():
     player.x +=held_keys['d'] * time.dt * 5
@@ -12,9 +34,18 @@ def update():
     player.y +=held_keys['w'] * time.dt * 5
     player.y -=held_keys['s'] * time.dt * 5
 
+    if distance_2d(player, food) < 0.7:
+        import random
+        food.x = random.uniform(-4,4)
+        food.y = random.uniform(-4,4)
+
+        player.scale += 0.5
+        print("Yummeh !")
+
     if held_keys['q']:
         application.quit()
-        
-EditorCamera()
+            
+camera.orthographic = True
+camera.fov = 10
 
 app.run()
